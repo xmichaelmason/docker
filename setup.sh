e73cd6ebcdf8752043fc80af0e6a9e4f20e40d09
Michael Mason <xmichaelmason@gmail.com>
2024-02-05 06:20:18 +0000

    cleanup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/docs \
+    ~/downloads \
+    ~/images \
+    ~/models \
+    ~/notebooks \
+    ~/storage/gitea/data \
+    ~/storage/gitea/etc/timezone \
+    ~/storage/gitea/etc/localtime \
+    ~/storage/grafana \
+    ~/storage/mariadb \
+    ~/storage/mongodb/data/db \
+    ~/storage/redis/data
